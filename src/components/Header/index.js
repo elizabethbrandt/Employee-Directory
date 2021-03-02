c9f4b0a11683fc0b4a90d0dfec89616b58721e60
Elizabeth Brandt <ebrandt8@uw.edu>
2021-03-02 00:06:23 -0800

    added styling

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,11 @@
+import "./style.css"
+
 function Header() {
     return (
         <div className="jumbotron jumbotron-fluid">
             <div className="container">
                 <h1 className="display-4">Employee Directory</h1>
-                <p className="lead">Search by name or sort by category</p>
+                <p className="lead">Search or sort by first name</p>
             </div>
         </div>
     )
